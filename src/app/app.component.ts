41a7519fa6e0ce643e51ef91320b695f042ff093
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-05-19 00:07:17 +0530

    V-16 basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,13 @@
 import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ChildComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name = signal<string>('Angular 16');
-
-  arr = signal([1, 2, 3, 4, 5]);
-
-  obj = signal({
-    firstName: 'sai',
-    lastName: 'kumar'
-  });
-
-  fullName = computed(() => `${this.obj().firstName} ${this.obj().lastName} ${this.name()}`)
-
-  changeName() {
-    // this.name = `${this.name} - ${Math.random()}`;
-    // this.name.set(`${this.name()} - ${Math.random()}`);
-    this.name.update((value) => `${value} - ${Math.random()}`)
-  }
-
-  updateArr() {
-    this.arr.mutate((value) => value.push(value.length + 1));
-  }
-
-  updateObj() {
-    this.obj.mutate((obj) => obj.firstName = 'updated');
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./login/login.component').then(c => c.LoginComponent)
-  },
-  {
-    path: 'login/:id',
-    loadComponent: () => import('./login/login.component').then(c => c.LoginComponent),
-    data: {
-      role: 'ADMIN'
-    }
-  }
-];
+export const routes: Routes = [];
